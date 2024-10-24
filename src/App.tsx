bfe47e219ffeca8cec5b9b76f3e54bc66a1acd33
Shivani Kanswal <shivanikanswal@gmail.com>
2024-10-24 12:06:50 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P];
+} & {
+  [P in Exclude<keyof T, K>]: T[P];
+};
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<
+    MyReadonly2<Todo, "title" | "description">[]
+  >([]);
+
+  const addTodo = () => {
+    const newTodo: Todo = {
+      title: "New Task",
+      description: "Task description",
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleCompletion = (index: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = {
+      ...updatedTodos[index],
+      completed: !updatedTodos[index].completed,
+    };
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TODO List</h1>
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.title} - {todo.description}
+            <button onClick={() => toggleCompletion(index)}>
+              {todo.completed ? "Undo" : "Complete"}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
